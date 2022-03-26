388c3e5a2de09d37ced73e5919bfdc1886f4d28f
moritzkoerber <koerber.moritz@gmail.com>
2022-03-26 12:23:04 +0100

    Add utils

--- Exercises/utils/utils.py
+++ Exercises/utils/utils.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 from awsume.awsumepy import awsume
 
 
 def get_assumed_role_boto3_session(profile_name):
     return awsume(profile_name)
+
+
+def create_directory(directory):
+    Path(directory).mkdir(parents=True, exist_ok=True)
