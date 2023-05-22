1c7d5e5844b7e2d8241ee1097af177b2d75dd83c
Moritz Körber <koerber.moritz@gmail.com>
2023-05-22 21:23:11 +0200

    Update utils

--- Exercises/utils/utils.py
+++ Exercises/utils/utils.py
@@ -6,24 +6,28 @@ from awsume.awsumepy import awsume
 from pyspark.sql import DataFrame, SparkSession
 
 
-def get_assumed_role_boto3_session(profile_name):
+def get_assumed_role_boto3_session(profile_name: str):
     return awsume(profile_name)
 
 
-def create_directory(directory):
+def create_directory(directory: str):
     Path(directory).mkdir(parents=True, exist_ok=True)
 
 
-def read_zipfile_content_into_memory(zipfile_path: Path):
-    with ZipFile(zipfile_path, "r") as zip:
-        return [zip.read(x) for x in zip.namelist() if re.fullmatch(r"[^/]*\.csv", x)]
-
-
-def extract_and_return_csv_filepaths(zip_file_path: Path):
-    with ZipFile(zip_file_path, "r") as zip:
+def read_zipfile_content_into_memory(zipfile_path: Path) -> list:
+    with ZipFile(zipfile_path, "r") as zip_file:
         return [
-            zip.extract(e, path="data")
-            for e in zip.namelist()
+            zip_file.read(x)
+            for x in zip_file.namelist()
+            if re.fullmatch(r"[^/]*\.csv", x)
+        ]
+
+
+def extract_and_return_csv_filepaths(zip_file_path: Path) -> list:
+    with ZipFile(zip_file_path, "r") as zip_file:
+        return [
+            zip_file.extract(e, path="data")
+            for e in zip_file.namelist()
             if re.fullmatch(r"[^/]*\.csv", e)
         ]
 
